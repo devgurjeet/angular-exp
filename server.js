f9f2290894f96a3597580bdf7f0d634f77d28d05
Gurjeet Singh <developer.gurjeet@gmail.com>
2017-03-17 21:41:29 +0530

    update package

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || '3080';
 app.set('port', port);
 
 /**
